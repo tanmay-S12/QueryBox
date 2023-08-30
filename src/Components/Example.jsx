dd0f41fc5bafdbad9c6a35b24c4b69dc02941035
tanmay-S12 <tanmaysankpal007@gmail.com>
2023-08-30 22:12:49 +0530

    changes in example 2

--- src/Components/Example.jsx
+++ src/Components/Example.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 
 const Example = () => {
-  return (
-    <div>
-        
-        <h1>hello example</h1>
-        <h1>hello example</h1>
-    </div>
-  )
+    return (
+        <div>
+
+            <h1>hello example</h1>
+            <h1>hello example</h1>
+            <h1>hello example</h1>
+            <h1>hello example</h1>
+            <h1>hello example</h1>
+            <h1>hello example</h1>
+        </div>
+    )
 }
 
 export default Example
